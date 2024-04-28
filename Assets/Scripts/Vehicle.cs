345afa7196605023144fe93ee4acb4833c951dbe
Lukas <39262485+lukaskornis@users.noreply.github.com>
2024-04-28 20:01:17 +0300

    Sounds

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -7,6 +7,23 @@ public class Vehicle : MonoBehaviour
 	public float speed = 1.0f;
 	public float rotateSpeed = 1.0f;
 
+	Rigidbody rb;
+	AudioSource engineSound;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		engineSound = GetComponent<AudioSource>();
+	}
+
+	void Update()
+	{
+		print( (int)(rb.velocity.magnitude * 3.6f) + " km/h");
+		var speedRatio = rb.velocity.magnitude / speed;
+
+		engineSound.pitch = 1 + speedRatio * 2;
+	}
+
 	public void Steer(float value)
 	{
 		transform.Rotate(0, value * rotateSpeed * Time.deltaTime, 0);
@@ -14,7 +31,7 @@ public class Vehicle : MonoBehaviour
 
 	public void Accelerate()
 	{
-		transform.position += transform.forward * speed * Time.deltaTime;
+		rb.velocity += transform.forward * speed * Time.deltaTime;
 	}
 
 	public void Brake()
