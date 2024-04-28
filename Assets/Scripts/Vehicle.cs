f8a0f5963b675a682e5d026c5e0984705544a25e
Lukas <39262485+lukaskornis@users.noreply.github.com>
2024-04-28 15:43:29 +0300

    Car Physics

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -4,10 +4,20 @@ public class Vehicle : MonoBehaviour
 {
 	public float speed = 10.0f;
 	public float turnSpeed = 100.0f;
+	public Rigidbody rb;
+	public AudioSource engineSound;
+
+	void Update()
+	{
+		print(rb.velocity.magnitude * 3.6f + " km/h");
+
+		var speedRatio = rb.velocity.magnitude / speed;
+		engineSound.pitch = 0.5f + 1.5f*speedRatio;
+	}
 
 	public void Accelerate()
 	{
-		transform.position += transform.forward * speed * Time.deltaTime;
+		rb.velocity += transform.forward * speed * Time.deltaTime;
 	}
 
 	public void Brake()
