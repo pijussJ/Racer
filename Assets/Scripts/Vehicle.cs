f07989b9d958fbbad157237eb23fa664a9403add
Lukas <39262485+lukaskornis@users.noreply.github.com>
2024-05-02 16:36:50 +0300

    Physics

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -2,22 +2,36 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {
-	public float speed = 10.0f;
+	public float maxSpeed;
+	public float acceleration = 10.0f;
 	public float turnSpeed = 100.0f;
 	public Rigidbody rb;
 	public AudioSource engineSound;
+	public float sideDrag;
+	public float drag;
 
 	void Update()
 	{
-		print(rb.velocity.magnitude * 3.6f + " km/h");
+		var sideFriction = Vector3.Dot(transform.right, rb.velocity.normalized);
+		rb.velocity += -transform.right * sideFriction * rb.velocity.magnitude * sideDrag * Time.deltaTime;
 
-		var speedRatio = rb.velocity.magnitude / speed;
+		var forwardFriction = Vector3.Dot(transform.forward, rb.velocity.normalized);
+		rb.velocity += -transform.forward * forwardFriction * rb.velocity.magnitude * drag * Time.deltaTime;
+
+
+		if (Mathf.Abs(sideFriction) > 0.3f)
+		{
+			print("drift");
+		}
+
+
+		var speedRatio = rb.velocity.magnitude / maxSpeed;
 		engineSound.pitch = 0.5f + 1.5f*speedRatio;
 	}
 
 	public void Accelerate()
 	{
-		rb.velocity += transform.forward * speed * Time.deltaTime;
+		rb.velocity += transform.forward * acceleration * Time.deltaTime;
 	}
 
 	public void Brake()
