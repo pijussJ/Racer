f51b7be6ae7ebde0ad8926fc906fe017d8c83ece
Lukas <39262485+lukaskornis@users.noreply.github.com>
2024-05-09 17:12:28 +0300

    Turning

--- Assets/Scripts/Brains.cs
+++ Assets/Scripts/Brains.cs
@@ -4,18 +4,30 @@ public class Brains : MonoBehaviour
 {
     Path path;
     Vehicle vehicle;
+    public int waypointIndex;
+    Vector3 nextPoint;
 
     void Start()
     {
         vehicle = GetComponent<Vehicle>();
         path = FindObjectOfType<Path>();
-        int waypoint = path.GetClosestWaypoint(transform.position);
-        print(waypoint);
+        waypointIndex = path.GetClosestWaypoint(transform.position);
+        nextPoint = path.GetWaypointPosition(waypointIndex);
     }
 
     void Update()
     {
-        var dir = Mathf.PerlinNoise1D(Time.time) * 2 - 1;
+        float dist = Vector3.Distance(transform.position, nextPoint);
+        Debug.DrawLine(transform.position, nextPoint,Color.red);
+        if (dist < 3)
+        {
+            waypointIndex = path.GetNextIndex(waypointIndex);
+            nextPoint = path.GetWaypointPosition(waypointIndex);
+        }
+
+
+        var nextPointDir = (nextPoint - transform.position).normalized;
+        var dir = Vector3.SignedAngle(transform.forward, nextPointDir, Vector3.up);
         vehicle.Turn(dir);
         vehicle.Accelerate();
     }
--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -1,12 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Path : MonoBehaviour
 {
-	public Transform[] waypoints;
+	public List<Transform> waypoints;
 
 	void Start()
 	{
-		waypoints = GetComponentsInChildren<Transform>();
+		waypoints = new List<Transform>(GetComponentsInChildren<Transform>());
+		waypoints.RemoveAt(0);
+	}
+
+	public int GetNextIndex(int i)
+	{
+		 if(i >= waypoints.Count) return 0;
+		 if(i < 0) return waypoints.Count - 1;
+		 else return i + 1;
+	}
+
+	public Vector3 GetWaypointPosition(int i)
+	{
+		if (i < 0 || i >= waypoints.Count) return Vector3.zero;
+		return waypoints[i].position;
 	}
 
 	public int GetClosestWaypoint(Vector3 position)
@@ -15,7 +30,7 @@ public class Path : MonoBehaviour
 		float closestDist = Vector3.Distance(position, closest.position);
 		int closestIndex = 0;
 
-		for (int i = 1; i < waypoints.Length; i++)
+		for (int i = 1; i < waypoints.Count; i++)
 		{
 			 float dist = Vector3.Distance(position, waypoints[i].position);
 			 if (dist < closestDist)
